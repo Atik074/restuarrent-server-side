7fc45a8fb7387da35f80533bdf2a47efbfbc9cf1
Atik074 <atiksmats#@gmail.com>
2023-08-30 23:45:54 +0600

    deleted api create and work it succesfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 
@@ -65,7 +65,13 @@ app.get('/carts', async(req,res)=>{
       res.send(result)
     })
 
-
+// delete data from cart
+   app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)} 
+      const result = await cartCollection.deleteOne(query) 
+        res.send(result)
+     })
 
 
     await client.db("admin").command({ ping: 1 });
