6e807ff468a8b09b69145a4f024120db53ac6e69
Atik074 <atiksmats#@gmail.com>
2023-09-10 23:02:55 +0600

    payment started work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRETE)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors')
